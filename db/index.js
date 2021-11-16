b60378e4c1501a8247dc7773ac56efdc71655f9e
Alan Alban <alan.alban10@gmail.com>
2021-11-15 21:07:16 -0500

    changed db model

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const User = require("./users");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 const db = mongoose.connect(process.env.MONGODB_URL);
 
 module.exports = {
--- db/users.js
+++ db/users.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true },
     password: { type: String, required: true },
-    cognitoToken: { type: String, required: true },
+    cognitoToken: { type: String, required: false },
   },
   {
     timestamps: true,
